6bf012cebf10217da1e25ac2f94f14e7f33d980c
Wongil <wongil@Wongilui-MacBookPro.local>
2020-02-28 13:26:37 +0900

    날씨 앱 완성

--- App.js
+++ App.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import Loading from './Loading';
+import Weather from './Weather';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+const API_KEY = "553e74623b90f36229adbf00f3b24f68";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+    
+  }
+  getWeather = async (latitude, longitude) => {
+    const { data: {main: {temp}, weather} } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    this.setState({
+      isLoading:false,
+      condition: weather[0].main,
+      temp
+    });
+  }
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords: { latitude, longitude }} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
+  }
 }
